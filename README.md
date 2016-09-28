f35a800fb87d08924ff3ab1caba2a0b9f3d5c4b6
Romeo Van Snick <romeovs@gmail.com>
2016-09-28 16:36:46 +0200

    Print info and add flags to control what to print

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ go get github.com/romeovs/jwt-cli
 - [ ] Add syntax highlighting to JSON
 - [x] Allow token to be piped in
 - [x] Allow filename as token arg, possibly parsing inside json
-- [ ] Print validity information
-- [ ] Add `-i` flag that only prints info
+- [x] Print validity information
+- [x] Add `-i` flag that only prints info
+- [x] Add `-j` flag that only prints json
 - [x] Add `-v` flag
 
 [jwt]: https://jwt.io
--- cmd/root.go
+++ cmd/root.go
@@ -6,13 +6,18 @@ import (
 	"io/ioutil"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/apex/log"
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gosuri/uitable"
 	"github.com/spf13/cobra"
 )
 
 var debug bool
+var onlyInfo bool
+var onlyJSON bool
+
 var RootCmd = &cobra.Command{
 	Use:   "jwt [token|path]",
 	Short: "jwt can be used the debug JWT tokens.",
@@ -21,6 +26,14 @@ var RootCmd = &cobra.Command{
 		if debug {
 			log.SetLevel(log.DebugLevel)
 			log.Debug("Debug on")
+		}
+
+		if onlyJSON {
+			log.Debug("Only printing JSON")
+		}
+
+		if onlyInfo {
+			log.Debug("Only printing info")
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
@@ -76,10 +89,37 @@ var RootCmd = &cobra.Command{
 			log.WithError(err).Fatal("Could not indent JSON")
 		}
 
-		fmt.Println(string(indented))
+		issued := time.Unix(int64(claims["iat"].(float64)), 0)
+		expires := time.Unix(int64(claims["exp"].(float64)), 0)
+		now := time.Now().Round(time.Second)
+
+		validity := "token is valid"
+		if issued.After(now) {
+			validity = fmt.Sprintf("token is not valid for %s", issued.Sub(now))
+		}
+		if now.After(expires) {
+			validity = fmt.Sprintf("token is expired for %s", now.Sub(expires))
+		}
+
+		table := uitable.New()
+		table.AddRow("  Issued:", issued)
+		table.AddRow("  Expires:", expires)
+		table.AddRow("  Valid:", validity)
+
+		fmt.Println()
+		if !onlyJSON {
+			fmt.Println(table)
+			fmt.Println()
+		}
+
+		if !onlyInfo {
+			fmt.Println(string(indented))
+		}
 	},
 }
 
 func init() {
 	RootCmd.PersistentFlags().BoolVarP(&debug, "verbose", "v", false, "show debug output")
+	RootCmd.PersistentFlags().BoolVarP(&onlyInfo, "info", "i", false, "only show info about token")
+	RootCmd.PersistentFlags().BoolVarP(&onlyJSON, "json", "j", false, "only show decoded token (no info)")
 }
